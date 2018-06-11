6a8331201c1b5820b881ee201fd528ff2e9d7be3
RevillWeb <leon@revillweb.com>
2018-06-11 17:56:05 +0100

    Update

--- lightwaverf.py
+++ lightwaverf.py
@@ -11,32 +11,32 @@ DOMAIN = 'lightwaverf'
 #set defaults incase of user error
 DEFAULT_NAME = 'Hub 1'
 DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 15672
+DEFAULT_PORT = 5672
 DEFAULT_STATE = False
-DEFAULT_RFLINK = '255.255.255.255'
+DEFAULT_LINK_IP = '255.255.255.255'
 
 #assign non known config
 #rabbitmq
 CONF_RABBIT_HOST = 'localhost'
-CONF_RABBITQUE = 'rabbitque'
-CONF_RABBITUNAME = 'rabbituname'
-CONF_RABBITPASS = 'rabbitpass'
-CONF_RABBITPORT = 'rabbitport'
-CONF_RFLINK = 'rflink'
+CONF_RABBIT_QUEUE = 'LightwaveRF'
+CONF_RABBIT_USER_NAME = 'username'
+CONF_RABBIT_PASS = 'password'
+CONF_RABBIT_PORT = 5672
+CONF_LINK_IP = '255.255.255.255'
 
 #validate user config
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_RABBIT_HOST, default=DEFAULT_HOST): cv.string,
-    vol.Optional(CONF_RABBITPORT, default=DEFAULT_PORT): cv.port,
-    vol.Required(CONF_RABBITQUE): cv.string,
-    vol.Required(CONF_RABBITUNAME): cv.string,
-    vol.Required(CONF_RABBITPASS): cv.string,
+    vol.Optional(CONF_RABBIT_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Required(CONF_RABBIT_QUEUE): cv.string,
+    vol.Required(CONF_RABBIT_USER_NAME): cv.string,
+    vol.Required(CONF_RABBIT_PASS): cv.string,
     vol.Optional(CONF_LIGHTS, default=[]):
         vol.All(cv.ensure_list, [
             vol.All({
                 vol.Required(CONF_ID): cv.string,
                 vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-                vol.Optional(CONF_RFLINK, default=DEFAULT_RFLINK): cv.string,
+                vol.Optional(CONF_LINK_IP, default=DEFAULT_LINK_IP): cv.string,
             })
         ])
 })
@@ -45,4 +45,4 @@ def setup(hass, config):
 
     load_platform(hass, 'light', DOMAIN)
     load_platform(hass, 'switch', DOMAIN)
-    hass.states.set('lightwaverf.LightwaveRF', f'Works! {CONF_RABBITHOST}')
+    hass.states.set('lightwaverf.LightwaveRF', f'Works! {CONF_RABBIT_HOST}')
