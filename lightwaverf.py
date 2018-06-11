40b7a866e48331605e5140b3c4aad2bb64a627db
RevillWeb <leon@revillweb.com>
2018-06-11 21:00:59 +0100

    Update

--- lightwaverf.py
+++ lightwaverf.py
@@ -1,10 +1,10 @@
 #import voluptuous as vol
 #import validation
-#import homeassistant.helpers.config_validation as cv
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
-#from homeassistant.components.light import (PLATFORM_SCHEMA)
+from homeassistant.components.light import (PLATFORM_SCHEMA)
 #import schema known light config
-#from homeassistant.const import (CONF_NAME, CONF_ID, CONF_LIGHTS)
+from homeassistant.const import (CONF_NAME, CONF_ID, CONF_LIGHTS)
 
 #Requirements
 REQUIREMENTS = ['pika==0.11.2']
@@ -28,24 +28,24 @@ CONF_RABBIT_PORT = 'rabbit_port'
 CONF_LINK_IP = 'link_ip'
 
 #validate user config
-# PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-#     vol.Optional(CONF_RABBIT_HOST, default=DEFAULT_HOST): cv.string,
-#     vol.Optional(CONF_RABBIT_PORT, default=DEFAULT_PORT): cv.port,
-#     vol.Required(CONF_RABBIT_QUEUE): cv.string,
-#     vol.Required(CONF_RABBIT_USERNAME): cv.string,
-#     vol.Required(CONF_RABBIT_PASS): cv.string,
-#     vol.Optional(CONF_LIGHTS, default=[]):
-#         vol.All(cv.ensure_list, [
-#             vol.All({
-#                 vol.Required(CONF_ID): cv.string,
-#                 vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-#                 vol.Optional(CONF_LINK_IP, default=DEFAULT_LINK_IP): cv.string,
-#             })
-#         ])
-# })
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_RABBIT_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Optional(CONF_RABBIT_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_RABBIT_QUEUE): cv.string,
+    vol.Required(CONF_RABBIT_USERNAME): cv.string,
+    vol.Required(CONF_RABBIT_PASS): cv.string,
+    vol.Optional(CONF_LIGHTS, default=[]):
+        vol.All(cv.ensure_list, [
+            vol.All({
+                vol.Required(CONF_ID): cv.string,
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                vol.Optional(CONF_LINK_IP, default=DEFAULT_LINK_IP): cv.string,
+            })
+        ])
+})
 
 def setup(hass, config):
     load_platform(hass, 'light', DOMAIN)
     #load_platform(hass, 'switch', DOMAIN)
-    conf = config.get('rabbit_pass')
+    conf = config.get(CONF_RABBIT_PASS)
     hass.states.set('lightwaverf.LightwaveRF', f'Works! {conf}')
