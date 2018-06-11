de44fc5645fa5df78275971d1d3a6236f9199626
RevillWeb <leon@revillweb.com>
2018-06-11 18:11:26 +0100

    Update

--- light/lightwaverf.py
+++ light/lightwaverf.py
@@ -24,6 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """ Setup LightWave RF lights """    
     devices = []
+    print(config)
     lights = config['lights']
     for light in lights:
         deviceid = light['id']
